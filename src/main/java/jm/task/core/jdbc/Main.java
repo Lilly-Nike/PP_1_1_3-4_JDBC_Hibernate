ca4da1999f83f07caa013b3f4de0041c95c69051
Dmitry Nikulin <d23nikulin@gmail.com>
2022-03-21 13:21:06 +0300

    Second commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,4 +1,5 @@
 package jm.task.core.jdbc;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
@@ -6,6 +7,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserService userService = new UserServiceImpl();
